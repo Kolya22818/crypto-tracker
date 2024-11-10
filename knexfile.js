bebfd3c040d57e9a0a966fa9f6cd6e0cfd16da17
Nicolay <87812125+Kolya22818@users.noreply.github.com>
2024-11-10 08:35:14 +0100

    Create knexfile.js

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+// knexfile.js
+
+require('dotenv').config();
+
+module.exports = {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+        directory: './migrations',
+    },
+};
+
